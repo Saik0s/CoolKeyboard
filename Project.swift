dcc30d20e0025950770d76f42c49f8f0a3122205
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-02-10 05:09:58 +0100

    Update project settings and debug flags
    This commit updates the project settings to include marketing version and debug flags. The debug configuration now includes the recommended settings for the JavaScript Standard Style and improved performance.

--- Project.swift
+++ Project.swift
@@ -3,25 +3,32 @@ import ProjectDescription
 
 let projectSettings: SettingsDictionary = [
   "GCC_TREAT_WARNINGS_AS_ERRORS": "YES",
-  "SWIFT_TREAT_WARNINGS_AS_ERRORS": "YES",
-  "OTHER_SWIFT_FLAGS[config=Debug][sdk=*][arch=*]": "-D DEBUG $(inherited) -Xfrontend -warn-long-function-bodies=500 -Xfrontend -warn-long-expression-type-checking=500 -Xfrontend -debug-time-function-bodies -Xfrontend -enable-actor-data-race-checks",
-  "OTHER_LDFLAGS[config=Debug][sdk=*][arch=*]": "$(inherited) -Xlinker -interposable -all_load",
+  // "SWIFT_TREAT_WARNINGS_AS_ERRORS": "YES",
   "CODE_SIGN_STYLE": "Automatic",
   "IPHONEOS_DEPLOYMENT_TARGET": "14.0",
+  "MARKETING_VERSION": "2.0.0",
+]
+
+let debugSettings: SettingsDictionary = [
+  "OTHER_SWIFT_FLAGS": "-D DEBUG $(inherited) -Xfrontend -warn-long-function-bodies=500 -Xfrontend -warn-long-expression-type-checking=500 -Xfrontend -debug-time-function-bodies -Xfrontend -enable-actor-data-race-checks",
+  "OTHER_LDFLAGS": "-Xlinker -interposable $(inherited)",
 ]
 
 let project = Project(
   name: "CoolKeyboard",
   options: .options(
+    disableShowEnvironmentVarsInScriptPhases: true,
     textSettings: .textSettings(
       indentWidth: 2,
       tabWidth: 2
     )
   ),
-  settings: .settings(configurations: [
-    .debug(name: "Debug", settings: projectSettings, xcconfig: nil),
-    .release(name: "Release", settings: projectSettings, xcconfig: nil),
-  ]),
+  settings: .settings(
+    base: projectSettings,
+    debug: debugSettings,
+    release: [:],
+    defaultSettings: .recommended
+  ),
   targets: [
     Target(
       name: "CoolKeyboard",
